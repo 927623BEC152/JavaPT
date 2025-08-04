3b172fef5bc5b618f406de7f594c06f04255c865
927623BEC152 <927623BEC152@mkce.ac.in>
2025-08-04 20:53:08 +0530

    created single.java

--- hello.java
+++ hello.java
@@ -1,5 +1,4 @@
 package JavaDay1;
-
 public class hello {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
---
+++ singleline.java
@@ -0,0 +1,10 @@
+package JavaDay1; 
+ 
+public class singleline 
+{	public static void main(String[] args) {
+	    int a=56;
+	    int b=12;
+	System.out.println("The value of A is: "+a + " The value of B is: "+b);
+	}
+}
+
